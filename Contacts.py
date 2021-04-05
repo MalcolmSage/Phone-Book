61e0d33888324b6811583d5bce9e1f600874f6d5
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-05 16:34:16 -0600

    Completed phonebook

--- Contacts.py
+++ Contacts.py
@@ -13,28 +13,39 @@ class People(BaseModel):
     first_name = CharField()
     number = CharField()
 
-# db.drop_tables([People])
 db.create_tables([People], safe=True)
 
 # People(first_name='Malcolm', number='555-777-3333').save()
+
+def prompts_ask():
+    selection = int(input("Return to the main menu? 1 = Yes or 2 = No: "))
+    if selection == 1:
+        prompts()
+    elif selection == 2:
+        print("Goodbye")
+    else:
+        prompts_ask()
 
 def prompts():
     print("Please read the following text, as the options have been changed")
     print("Select 1 for a list of all your contacts")
     print("Select 2 for to search for a specific person")
-    print("Select 3 to add a new friend")
+    print("Select 3 to add a new contact")
     print("Select 4 to close this phonebook")
     selection = int(input("Please choose now: "))
     while selection < 1 or selection > 4:
         print("Invalid selection")
         selection = int(input("Please choose again: "))
+    
+    # List
     if selection == 1:
         print("All Contacts")
         people = People.select()
         for person in people:
             print(f'{person.first_name}, {person.number}')
-        prompts()
+        prompts_ask()
 
+    # Searching
     elif selection == 2:
         print("Contact search")
         print("Who are you looking for?")
@@ -50,15 +61,14 @@ def prompts():
             found = People.select().where(People.first_name == search)
             for person in found:
                 print(f'{person.first_name}, {person.number}')
-                prompts()
-            
-        # selected = People.select().where(People.first_name == search)
 
-
-
+    # Create
     elif selection == 3:
-        print("New friend")
-
+        print("New Contact")
+        add_first_name = input("First Name: ")
+        add_number = input("Number: ")
+        People(first_name=add_first_name, number=add_number).save()
+        prompts_ask()
 
 
     else:
